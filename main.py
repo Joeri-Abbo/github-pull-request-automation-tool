ab624f9da6217fd4622dd0b879d2f6fcce4dfde8
Joeri Abbo <joeriabbo@hotmail.com>
2023-11-22 20:02:15 +0100

    Speedup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from github import Github
 from dotenv import load_dotenv
 
@@ -10,31 +11,15 @@ if github_token is None:
 
 g = Github(github_token)
 
-repo_owner = input("Enter the name of the user or organization whose repositories you want to check:")
-all_repos = []
 
-try:
-    if g.get_user().login == repo_owner:
-        repos = g.get_user().get_repos()
-    else:
-        repos = g.get_organization(repo_owner).get_repos()
-
-    all_repos.extend(repos)
-
-    while repos._couldGrow():
-        repos = repos._grow()
-        all_repos.extend(repos)
-
-    for repo in all_repos:
+def process_repo(repo):
+    try:
         if repo.private:
             print(f"Private repository: {repo.full_name}")
         else:
             print(f"Public repository: {repo.full_name}")
 
         pull_requests = repo.get_pulls(state='open')
-        if pull_requests.totalCount == 0:
-            print("No open pull requests found.")
-            continue
         for pull_request in pull_requests:
             check_runs = g.get_repo(repo.full_name).get_commit(pull_request.head.sha).get_check_runs()
             checks_passed = all(
@@ -51,5 +36,27 @@ try:
             else:
                 print(
                     f"Not all status checks have passed for pull request #{pull_request.number} in repository {repo.full_name}. It will not be merged.")
+    except Exception as e:
+        print(f"Error processing repository {repo.full_name}: {e}")
+
+
+repo_owner = input("Enter the name of the user or organization whose repositories you want to check:")
+threads = []
+
+try:
+    if g.get_user().login == repo_owner:
+        repos = g.get_user().get_repos()
+    else:
+        repos = g.get_organization(repo_owner).get_repos()
+
+    for repo in repos:
+        thread = threading.Thread(target=process_repo, args=(repo,))
+        threads.append(thread)
+        thread.start()
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
 except Exception as e:
     print(f"Error fetching repositories: {e}")
